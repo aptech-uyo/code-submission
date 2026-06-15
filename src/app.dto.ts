609ed846446609087ecb353d1db65ad38f127939
Bennet Otoho <bennet2000@outlook.com>
2026-06-15 20:13:04 +0100

    define code submission form DTOs, set up CSRF protection

--- src/app.dto.ts
+++ src/app.dto.ts
@@ -1,7 +1,36 @@
+import { IsNotEmpty, IsNumber } from 'class-validator'
+
 export interface Page {
   pageTitle: string
 }
 
-export interface QuestionPage extends Page {
+export interface CsrfProtectedPage extends Page {
+  csrfToken: string // for forms and POST requests
+}
+
+export interface QuestionPage extends CsrfProtectedPage {
   questionId: number
 }
+
+export interface SubmissionsPage extends Page {
+  submissions: SubmissionReportDto[]
+}
+export class SubmissionResponseDto {
+  @IsNumber()
+  studentId!: number
+
+  @IsNotEmpty()
+  codeText!: string
+}
+
+interface SubmissionReportDto {
+  submissionId: number
+  studentId: number
+  executionId: number
+  studentName: string
+  language: string
+  latestStatus: 'SUCCESS' | 'COMPILE_ERROR' | 'RUNTIME_ERROR' | 'WRONG_OUTPUT'
+}
+
+export const SESSION_COOKIE_NAME = 'connect.sid'
+export const CSRF_HEADER_NAME = 'X-Csrf-Token'
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,40 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import cookieParser from 'cookie-parser'
+import { csrfSync } from 'csrf-sync'
+import session from 'express-session'
 import hbs from 'hbs'
-import { join } from 'path'
-import { AppModule } from './app.module'
 // @ts-expect-error 'hbs-utils' has no type declarations
 import hbsUtils from 'hbs-utils'
+import { join } from 'path'
+
+import { CSRF_HEADER_NAME, SESSION_COOKIE_NAME } from './app.dto'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const configService = app.get(ConfigService)
 
   app.useStaticAssets(join(__dirname, '..', 'public'))
   app.setBaseViewsDir(join(__dirname, '..', 'views'))
+
+  // session
+  app.use(
+    session({
+      cookie: { secure: false, maxAge: 30 * 24 * 60 * 60 * 1000 },
+      name: SESSION_COOKIE_NAME,
+      proxy: false,
+      secret: configService.getOrThrow<string>('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+  app.use(cookieParser(configService.getOrThrow<string>('SESSION_SECRET')))
+  const { csrfSynchronisedProtection } = csrfSync({
+    getTokenFromRequest: (req) => req.header(CSRF_HEADER_NAME)
+  })
+  app.use(csrfSynchronisedProtection)
 
   // Handlebars
   app.setViewEngine('hbs')
